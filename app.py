a44d803a8a28c351a9dd94c73a3279d5903fc4dd
SriBalina <balinarathnasri@gmail.com>
2023-04-09 00:57:48 +0530

    list added

--- app.py
+++ app.py
@@ -1,5 +1,26 @@
-from flask import Flask
+from flask import Flask,request
+
 app = Flask(__name__)
+
+hackathons = {
+    "GHW: API Week": {
+        "start_date": "2023-04-03 12:00:00",
+        "end_date": "2023-04-10 12:00:00",
+        "location": "Everywhere, Online",
+        "type": "Digital Only"
+    },
+    "Bitcamp": {
+        "start_date": "2023-04-07 12:00:00",
+        "end_date": "2023-04-09 12:00:00",
+        "location": "College Park, Maryland",
+        "type": "In-Person Only"
+    }
+}
+
 @app.route("/")
 def hello_ghw():
     return "<p>HELLO WORLD</p>"
+
+@app.route('/Hackathon',methods=['GET'])
+def Hackathon():
+    return hackathons;
